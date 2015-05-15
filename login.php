355a127e1387d48022434f57ea3c702875912b5f
Olena Likhytska <olenalikhytska@gmail.com>
2015-05-15 17:37:52 +0300

    second commit

--- login.php
+++ login.php
@@ -3,19 +3,37 @@ session_start();
 ?>
 <html>
 <body>
-	<a href='session.php'>Click Here for your Profile</a>	
+	<a href = 'session.php'>Click Here for your Profile</a>	
+	<br/>
+	<a href = 'login.php?logout=1'>Click here to Logout</a> 
 <?php
 $dbName ="Lanett";
 $dbPass ="12345";
 
-if(isset($_POST['submit'])){
+$requestLogout = isset($_GET['logout']);
+$requestLogin = isset($_POST['submit']);
+
+function login($userName){
+	$_SESSION['CurrentUser'] = $userName;
+	$_SESSION['CurrentAcessLevel'] = 5;
+	echo "Welcome back, " . $userName;
+}
+
+function logout(){
+	session_unset();
+	session_destroy();
+	echo "you are logged out";
+}
+if($requestLogout){
+	logout();
+}
+if($requestLogin){
 	
 	$user = $_POST['user'];
 	$pass = $_POST['pass'];
 if($user == $dbName ){
 	if($pass == $dbPass){
-		$_SESSION['CurrentUser'] = $user;
-		echo "Welcome back, " . $user;
+		login($user);
 	}
 	else{
 		Mess("Wrong password!");
